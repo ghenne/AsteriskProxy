6ff771b7257dffbf0e325bf316cb083d6759b081
Gunnar Henne <henne@otris.de>
2010-09-30 09:07:55 +0200

    Umwandlung in VS2008 Projekt

---
+++ AsteriskProxy/AsteriskProxy.vb
@@ -0,0 +1,51 @@
+﻿Module AsteriskProxy
+    Public Class UserAgentLoginEvent
+        Inherits Asterisk.NET.Manager.Action.LoginAction
+        Public Agent As String
+    End Class
+    Dim manager As Asterisk.NET.Manager.ManagerConnection
+    WithEvents managerEvt As Asterisk.NET.Manager.ManagerConnection
+    Sub dam_UserEvents(ByVal sender As Object, ByVal userEvent As Object) Handles managerEvt.UserEvents
+
+        If (TypeOf userEvent Is UserAgentLoginEvent) Then
+            Console.WriteLine("User Event - AgentLogin:" + "\n\tAgent\t\t")
+            Console.WriteLine(CType(userEvent, UserAgentLoginEvent).Agent)
+        Else
+            Console.WriteLine("User Event:" + "\n\tUserEventName\t\t" + userEvent.UserEventName)
+            '  foreach (System.Collections.Generic.KeyValuePair<string, string> pair in e.Attributes)
+            '{
+            'Console.WriteLine(String.Format("\t{0}\t{1}", pair.Key, pair.Value));
+            '}
+        End If
+    End Sub
+
+
+    Const DEV_HOST As String = "192.168.7.144"
+    Const ASTERISK_PORT As Integer = 5038
+    Const ASTERISK_HOST As String = "192.168.7.199"
+    Const ASTERISK_LOGINNAME As String = "phonesuite"
+    Const ASTERISK_LOGINPWD As String = "5d4aba078"
+
+    Const ORIGINATE_CONTEXT As String = "from-internal"
+    Const ORIGINATE_CHANNEL As String = "IAX2/100"
+    Const ORIGINATE_EXTRA_CHANNEL As String = "SIP/101"
+    Const ORIGINATE_EXTRA_EXTEN As String = "101"
+    Const ORIGINATE_EXTEN As String = "101"
+    Const ORIGINATE_CALLERID As String = "Asterisk.NET"
+    Const ORIGINATE_TIMEOUT As Integer = 15000
+    Sub Main()
+        manager = New Asterisk.NET.Manager.ManagerConnection(ASTERISK_HOST, ASTERISK_PORT, ASTERISK_LOGINNAME, ASTERISK_LOGINPWD)
+        'Register user event class
+        manager.RegisterUserEventClass(GetType(UserAgentLoginEvent))
+        Try
+
+            manager.Login()         ' Login only (fast)
+            Console.WriteLine("Asterisk version : " + manager.Version)
+        Catch ex As System.Exception
+            Console.WriteLine(ex)
+            Console.ReadLine()
+            manager.Logoff()
+        End Try
+    End Sub
+
+End Module
---
+++ AsteriskProxy/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die mit einer Assembly verknüpft sind.
+
+' Die Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("AsteriskProxy")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("otris software AG")> 
+<Assembly: AssemblyProduct("AsteriskProxy")> 
+<Assembly: AssemblyCopyright("Copyright © otris software AG 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+<Assembly: Guid("ff79727b-02ff-4c1a-91cc-23dc2bdcc6d1")> 
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion 
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" eingeben:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
